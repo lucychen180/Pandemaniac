09d846c9537233065ea73811341e84e9008ab230
Lucy Chen <lhchen@caltech.edu>
2020-02-23 22:47:12 -0800

    slight randomness to degree.py + 2-23 runs

--- degree.py
+++ degree.py
@@ -5,12 +5,13 @@ import networkx as nx
 import numpy as np
 
 if len(sys.argv) == 1:
-    print("usage: pandy.py [graph filename]")
+    print("usage: pandy.py [graph filename] [num_seeds]")
     sys.exit(1)
 
 # filename format: num_players.num_seeds.unique_id
 file_info = sys.argv[1].split('.')
 num_seeds = int(file_info[1])
+# num_seeds = int(sys.argv[2])
 
 # Construct undirected graph
 G = nx.Graph()
@@ -26,10 +27,20 @@ print('number of edges:', G.number_of_edges())
 
 # Find seed nodes based on degree
 node_degs = G.degree()  # contains tuples (node, deg) of type (str, int)
-seeds = heapq.nlargest(num_seeds, node_degs, key=(lambda pair : pair[1]))
+seed_pool = heapq.nlargest(2*num_seeds, node_degs, key=(lambda pair : pair[1]))
+seed_pool = np.array(seed_pool)
 
 # Output seed nodes
-with open('seeds.txt', 'w') as f:
+out_file = '.'.join(file_info[:-1]) + '_seeds.txt'
+with open(out_file, 'w') as f:
     for round in range(50):
-        for node, deg in seeds:
+        # guarantee best seeds
+        top = min(3, num_seeds)
+        top_pool = seed_pool[:top, 0]
+        for node in top_pool:
+            print(node, file=f)
+        # randomly pick remaining seeds
+        random_pool = np.random.choice(seed_pool[top:2*num_seeds-3, 0],
+                                       size=(num_seeds-top), replace=False)
+        for node in random_pool:
             print(node, file=f)
